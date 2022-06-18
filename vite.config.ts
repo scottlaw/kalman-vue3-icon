2f62bff99be806acdce3be7b230ded5dfee2bcae
Scott <rabblerlau@gmail.com>
2022-06-18 18:47:51 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'kalman-vue3-icon',
+      fileName: (format) => `kalman-icon.${format}.ts`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
