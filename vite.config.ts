92bac0016f90f7381e4999db4400146951267754
Scott <rabblerlau@gmail.com>
2022-06-20 10:46:26 +0800

    change: update MVP version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'kalman-vue3-icon',
       fileName: (format) => `kalman-icon.${format}.ts`
     },
