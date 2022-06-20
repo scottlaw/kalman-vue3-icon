906711b05ac7a3ed62e92b06f63fc6722966de66
Scott <rabblerlau@gmail.com>
2022-06-20 19:07:01 +0800

    feat: add demo page.

--- src/icons/circle.vue
+++ src/icons/circle.vue
@@ -1,9 +1,9 @@
 
   <template>
     <svg
-      xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" aria-hidden="true" :stroke="color" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+      xmlns="http://www.w3.org/2000/svg" :width="size" :height="size" viewBox="0 0 24 24" aria-hidden="true" :fill="color"
     >
-      <path fillRule="evenodd" clipRule="evenodd" d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"></path>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"></path>
     </svg>
   </template>
   <script>
