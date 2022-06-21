c2cb718c3258d764f58dc767d4e0f3bbb7a22faa
Scott <rabblerlau@gmail.com>
2022-06-21 13:51:12 +0800

    change: modify the bundle path.

--- bundle.config.ts
+++ bundle.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/kalman-vue3-icon/',
   plugins: [vue(), dts(),],
   resolve: {
     alias: {
