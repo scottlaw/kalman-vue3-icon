da5298ea77e64e4030d8491d43f2c4ebca288765
Scott <rabblerlau@gmail.com>
2022-06-21 11:35:14 +0800

    change: modify base url config.

--- bundle.config.ts
+++ bundle.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/kalman-vue3-icon/',
   plugins: [vue(), dts(),],
   resolve: {
     alias: {
